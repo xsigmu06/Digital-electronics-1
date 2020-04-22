f3524e38d997b98300138b53965dc4b4f59e2500
michalsvento <xsvent00@stud.feec.vutbr.cz>
2020-04-22 19:00:37 +0200

    v2

--- Labs/Project/hc-sr04/hc_sr04.vhd
+++ Labs/Project/hc-sr04/hc_sr04.vhd
@@ -19,7 +19,8 @@
 ----------------------------------------------------------------------------------
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
-use IEEE.numeric_std.all;
+use IEEE.std_logic_arith.all;
+use IEEE.std_logic_unsigned.all;
 
 -- Uncomment the following library declaration if using
 -- arithmetic functions with Signed or Unsigned values
@@ -32,71 +33,93 @@ use IEEE.numeric_std.all;
 
 entity hc_sr04 is
     Port ( clk_i : in  STD_LOGIC;
-           srst_n_i : in  STD_LOGIC;
+           srst_n_i : in  STD_LOGIC;  -- active low
            echo_i : in  STD_LOGIC;
-           trig_o : out  STD_LOGIC;
-		   dstnc_o : out STD_LOGIC_VECTOR(11 downto 0) -- 4000 mm
+           trig_o : out  STD_LOGIC :='0' ;
+		   dstnc_o : out STD_LOGIC_VECTOR (11 downto 0) := "000000000000" -- 4000 mm
 		   );
 end hc_sr04;
 
 architecture Behavioral of hc_sr04 is
 		
-		type state_type is (Trigger, 40kHz, Echo);
+		type state_type is (Trigger, Pulse, Echo, Calc, Reset);
 	
 		signal s_state: state_type;
-		signal s_cnt: unsigned(14 downto 0) := x"0"; -- (2* 4m) / 340m/s = 23 530 us -> 2^15= 32 768
-		signal sound: unsigned(14 downto 0);
-		
-		constant 10us : unsigned(3 downto 0) := "1010";
+		signal s_cntEcho: unsigned (14 downto 0) := "000000000000000"; -- (2* 4m) / 340m/s = 23 530 us -> 2^15= 32 768
+		signal s_cntTrig: unsigned (3 downto 0) :="0000";
+		signal sound: unsigned (14 downto 0);
+		signal result : STD_LOGIC_VECTOR(30 downto 0);
+		signal s_en : STD_LOGIC;
+
+		constant trigstart : unsigned (3 downto 0) := "1010";
+		constant soundspeed : STD_LOGIC_VECTOR(15 downto 0) := "0010101110000101"; -- mm/us  /2
 		
 	begin
 	
+	CLK_EN : entity work.clock_enable
+	 generic map (
+        g_NPERIOD => x"0001"        -- @ 1 us if fclk = 1 MHz
+    )
+    port map (
+    srst_n_i => srst_n_i,
+    clk_i => clk_i,
+    clock_enable_o => s_en
+    );
 	
 	
-	soundmeasure: process(clk_i)
+	
+	soundmeasure: process(clk_i )
 		begin
 			if rising_edge(clk_i) then
 				if srst_n_i = '0' then 	-- synchronous reset, active low
-					s_cnt <= (others => '0');
+					s_cntTrig <= (others => '0');
+					s_cntEcho <= (others => '0') ;
 					s_state <= Trigger;	-- state 0
 					
-				else
+				elsif s_en = '1' then
 					case s_state is
 						 
 						 when Trigger =>
-								if s_cnt < 10us then
+								if s_cntTrig < trigstart then
+									trig_o <= '1';
 									s_state <= Trigger;
-									trig_i <= '1';
-									s_cnt <= s_cnt + 1;
+									s_cntTrig <= s_cntTrig + x"1";
 								else 
-									s_state <= 40kHz
-									s_cnt <= x"0";
-									trig_i <= '0';
-									
+									trig_o <= '0';
+									s_state <= Pulse;
+									s_cntTrig <= "0000" ;
 								end if;
 								
-						when 40kHz => 
-								if rising_edge(echo_i) then
+						when Pulse => 
+								if echo_i = '1' then
+									s_cntEcho <= s_cntEcho + 1;
 									s_state <= Echo;
 								else
-									s_state <= 40kHz
+									s_state <= Pulse;
 								end if;
 						when Echo => 
 								if echo_i = '1' then
 									s_state <= Echo;
-									s_cnt <= s_cnt + 1;
+									s_cntEcho <= s_cntEcho + 1;
 								else 
+									s_state <= calc;
+									sound <=  s_cntEcho;
+								end if;
+						when Calc => 		
+									result <= unsigned(sound) * unsigned(soundspeed);		
+									dstnc_o <= result(27 downto 16);
+									s_state <= Reset;
+						when Reset =>
 									s_state <= Trigger;
-									sound <=  s_count;
-						
+									s_cntEcho <= "000000000000000";
+									
 						when others => 
 									s_state <= Trigger;
 					end case;
 				end if;
 			end if;
 		end process soundmeasure;
-								
-	
+			
 
 end Behavioral;
 
---
+++ Labs/Project/hc-sr04/hc_sr04_tb00.vhd
@@ -0,0 +1,104 @@
+--------------------------------------------------------------------------------
+-- Company: 
+-- Engineer:
+--
+-- Create Date:   18:18:04 04/22/2020
+-- Design Name:   
+-- Module Name:   F:/projekty_de/hc-sr04/hc_sr04_tb00.vhd
+-- Project Name:  hc-sr04
+-- Target Device:  
+-- Tool versions:  
+-- Description:   
+-- 
+-- VHDL Test Bench Created by ISE for module: hc_sr04
+-- 
+-- Dependencies:
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+--
+-- Notes: 
+-- This testbench has been automatically generated using types std_logic and
+-- std_logic_vector for the ports of the unit under test.  Xilinx recommends
+-- that these types always be used for the top-level I/O of a design in order
+-- to guarantee that the testbench will bind correctly to the post-implementation 
+-- simulation model.
+--------------------------------------------------------------------------------
+LIBRARY ieee;
+USE ieee.std_logic_1164.ALL;
+ 
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--USE ieee.numeric_std.ALL;
+ 
+ENTITY hc_sr04_tb00 IS
+END hc_sr04_tb00;
+ 
+ARCHITECTURE behavior OF hc_sr04_tb00 IS 
+ 
+    -- Component Declaration for the Unit Under Test (UUT)
+ 
+    COMPONENT hc_sr04
+    PORT(
+         clk_i : IN  std_logic;
+         srst_n_i : IN  std_logic;
+         echo_i : IN  std_logic;
+         trig_o : OUT  std_logic;
+         dstnc_o : OUT  std_logic_vector(11 downto 0)
+        );
+    END COMPONENT;
+    
+
+   --Inputs
+   signal clk_i : std_logic := '0';
+   signal srst_n_i : std_logic := '0';
+   signal echo_i : std_logic := '0';
+
+ 	--Outputs
+   signal trig_o : std_logic;
+   signal dstnc_o : std_logic_vector(11 downto 0);
+
+   -- Clock period definitions
+   constant clk_i_period : time := 10 ns;
+ 
+BEGIN
+ 
+	-- Instantiate the Unit Under Test (UUT)
+   uut: hc_sr04 PORT MAP (
+          clk_i => clk_i,
+          srst_n_i => srst_n_i,
+          echo_i => echo_i,
+          trig_o => trig_o,
+          dstnc_o => dstnc_o
+        );
+
+   -- Clock process definitions
+   clk_i_process :process
+   begin
+		clk_i <= '0';
+		wait for clk_i_period/2;
+		clk_i <= '1';
+		wait for clk_i_period/2;
+   end process;
+ 
+
+   -- Stimulus process
+   stim_proc: process
+   begin		
+      -- hold reset state for 100 ns.
+      wait for 100 ns;	
+		srst_n_i <= '1';
+      wait for clk_i_period*10;
+
+      -- insert stimulus here  
+      wait for clk_i_period*50;
+	  echo_i <= '1';
+	  wait for clk_i_period*100;
+	  echo_i <= '0';
+
+
+      wait;
+   end process;
+
+END;
